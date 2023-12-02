0bcfe5f53acbee25726c91c41ba69ab9d71c4584
white <abhijithkkpurameri@gmail.com>
2023-12-02 21:59:51 +0530

    footer section completed

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -3,7 +3,41 @@ import './Footer.css'
 const Footer = () => {
   return (
     <div className='footer'>
-      
+      <div className="f-container">
+
+      <div className="f-top">
+    <div className="top-item1">
+    <div className="f-h1">
+          Headquarts
+        </div>
+        <div className="f-sml">
+          Muscat,Oman
+        </div>
+    </div>
+    <div className="top-item1">
+    <div className="f-h1">
+          Headquarts
+        </div>
+        <div className="f-sml">
+          Muscat,Oman
+        </div>
+    </div>
+    <div className="top-item1">
+    <div className="f-h1">
+          Headquarts
+        </div>
+        <div className="f-sml">
+          Muscat,Oman
+        </div>
+    </div>
+      </div>
+      <hr className='line' />
+      <div className="f-bottom">
+        
+        <div>&copy;OQ HSSE Symposium 2023.</div>
+        <div><span>&gt;</span> oq.com | @thisoq</div>
+      </div>
+      </div>
     </div>
   )
 }
